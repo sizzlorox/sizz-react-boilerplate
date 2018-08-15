867f72aecd9bd4bae8d412203bae082996a3b819
sizzlorr <sizzlorox@gmail.com>
2018-08-15 17:02:06 +0800

    fix(card): front back animation now working

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -10,6 +10,10 @@ class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <div className={classnames('home-content')}>
+          This was made for me to keep a well structured boilerplate for personal use.
+          It's intended to keep being updated as I learn more about structuring React Apps.
+        </div>
         <Card cardTitle='Minimal'>As minimal as can be.</Card>
         <Card cardTitle='Easy'>Made to be easy to change and maintain</Card>
         <Card cardTitle='Practice'>Made to practice more!</Card>
