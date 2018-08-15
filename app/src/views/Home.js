fe0f4f78741694538d439a49ada25a506eef879c
sizzlorr <sizzlorox@gmail.com>
2018-08-15 17:19:06 +0800

    add(navbar): simple animation to links

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -13,10 +13,11 @@ class Home extends React.Component {
         <div className={classnames('home-content')}>
           This was made for me to keep a well structured boilerplate for personal use.
           It's intended to keep being updated as I learn more about structuring React Apps.
+          I'm trying to get as much knowledge as I can with ReactJS's best practices and applying it here.
         </div>
         <Card cardTitle='Minimal'>As minimal as can be.</Card>
         <Card cardTitle='Easy'>Made to be easy to change and maintain</Card>
-        <Card cardTitle='Practice'>Made to practice more!</Card>
+        <Card cardTitle='Practice'>Made for practice!</Card>
       </React.Fragment>
     );
   }
