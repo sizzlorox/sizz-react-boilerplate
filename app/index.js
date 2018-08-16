4e31109550bb0ed0cc21e170cd05f02b3d881004
sizzlorr <sizzlorox@gmail.com>
2018-08-16 12:26:54 +0800

    add(index): serviceWorker

--- app/index.js
+++ app/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 // Main Component
 import App from './src/App';
 
+// Service Worker
+import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
 ), document.getElementById('app'), (err) => console.log(err));
 
+registerServiceWorker();
 if (module.hot) {
   module.hot.accept();
 }
