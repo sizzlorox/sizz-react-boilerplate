4d25368aa4a73b7daf882fb643b5aa96cd653e48
sizzlorr <sizzlorox@gmail.com>
2018-08-17 19:15:29 +0800

    add(footer): content to footer

--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -3,8 +3,27 @@ export default () => {
     <div className={classnames('footer')}>
       <div className={classnames('footer-bg')}>
         <div className={classnames('footer-content')}>
-          <p>Copyright © {new Date().getFullYear()} Something something</p>
+          <div className={classnames('footer-sitemap')}>
+            <p>Site Map</p>
+            <hr />
+            <ul>
+              <li>
+                Home
+              </li>
+              <li>
+                404
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p>Contact Us</p>
+            <hr />
+            Email: sizzlorox@gmail.com
+            <br />
+            Phone: (XX) XXXX-XXXX
+          </div>
         </div>
+        <p className={classnames('footer-copyright')}>Copyright © {new Date().getFullYear()} Something something</p>
       </div>
     </div>
   );
